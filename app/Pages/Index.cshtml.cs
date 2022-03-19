02a4036f586fc224a7aa8057b37b7d34d9400393
Ana Cozma <ana.cozma@lobsterink.com>
2022-03-19 17:38:23 +0100

    Initial setup
    - enable HPA for the deployment and sets the target utilization values
    - sets CPU request to 150m
    - increases replica count to 3
    - configures ingress
    - make Hello world message parametric
    - add dotnet .gitignore file

--- app/Pages/Index.cshtml.cs
+++ app/Pages/Index.cshtml.cs
@@ -6,14 +6,19 @@ namespace MyWebApp.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _configuration;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public void OnGet()
     {
 
     }
+
+    public string Greeting => _configuration.GetValue<string>("GREET_VAR");
+    
 }
